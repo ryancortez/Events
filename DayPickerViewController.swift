b0250a9501026631cc31387d6b522958cdd481b0
Ryan Cortez <ryancortez@me.com>
2016-07-11 20:08:54 -0500

    - Refactored DatePickerView

--- DayPickerViewController.swift
+++ DayPickerViewController.swift
@@ -7,24 +7,56 @@
 //
 
 import UIKit
+import EventKitUI
 
 class DayPickerViewController: UIViewController, RSDFDatePickerViewDelegate, RSDFDatePickerViewDataSource{
 
     var dateFromRSDF:NSDate = NSDate()
     
     override func viewWillAppear(animated: Bool) {
-         self.navigationController?.navigationBarHidden = false
+        setViewDefaults()
+        createDatePickerView()
     }
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    func setViewDefaults() {
+        self.navigationController?.navigationBarHidden = false
         self.navigationItem.backBarButtonItem?.setTitleTextAttributes([NSFontAttributeName: UIFont(name: "Avenir", size: 18)!]
             , forState: UIControlState.Normal)
-            let datePickerView = RSDFDatePickerView(frame: self.view.bounds)
-                datePickerView.delegate = self
-                datePickerView.dataSource = self
-                self.view.addSubview(datePickerView)
-            }
+    }
+    
+    func createDatePickerView() {
+        let datePickerView = RSDFDatePickerView(frame: self.view.bounds)
+        datePickerView.delegate = self
+        datePickerView.dataSource = self
+        self.view.addSubview(datePickerView)
+    }
+    
+    func checkForCalendarAccess() {
+        let eventStore = EKEventStore()
+        switch EKEventStore.authorizationStatusForEntityType(EKEntityType.Event) {
+        case .Authorized:
+            // If you already have access to the Calendar
+            print()
+        case .Denied:
+            // If you were not given access to the Calendar
+            print()
+        case .NotDetermined:
+            // If it hasn't been asked to the user yet
+            
+            // Request permisson to the calendar
+            eventStore.requestAccessToEntityType(EKEntityType.Event, completion:
+                {(granted: Bool, error: NSError?) -> Void in
+                    if granted {
+                        
+                    } else {
+                        // If you were not given access to the Calendar by the user
+                    }
+            })
+        default:
+            print("Default Case")
+        }
+    }
+
     
     func datePickerView(view: RSDFDatePickerView!, didSelectDate date: NSDate!) {
         dateFromRSDF = date
